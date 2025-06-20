5acf858134b1ef5eebeeae36a8ce31fd8c7de0e0
Giovanna Fioravante Dalledone <gfd23@inf.ufpr.br>
2025-06-20 00:48:53 -0300

    movimentação funcionando

--- jogo_testes.c
+++ jogo_testes.c
@@ -7,8 +7,40 @@ int main ()
 {
     struct tabuleiro_t *tabuleiro = cria_tabuleiro();
     struct jogador_t *jogador = cria_jogador();
+    unsigned int movimentacao;
 
     imprime_tabuleiro(tabuleiro, jogador);
+
+    printf("------------------------------------------------------------------------------\n");
+
+    //Cima
+    movimentacao = movimenta_jogador(tabuleiro, jogador, 11);
+    imprime_tabuleiro(tabuleiro, jogador);
+
+    printf("------------------------------------------------------------------------------\n");
+
+    //Direita
+    movimentacao = movimenta_jogador(tabuleiro, jogador, 10);
+    imprime_tabuleiro(tabuleiro, jogador);
+
+    printf("------------------------------------------------------------------------------\n");
+    //Baixo
+    movimentacao = movimenta_jogador(tabuleiro, jogador, 12);
+    imprime_tabuleiro(tabuleiro, jogador);
+
+    printf("------------------------------------------------------------------------------\n");
+
+    //Esquerda
+    movimentacao = movimenta_jogador(tabuleiro, jogador, 13);
+    imprime_tabuleiro(tabuleiro, jogador);
+
+    printf("------------------------------------------------------------------------------\n");
+
+    //Movimentação inválida
+    movimentacao = movimenta_jogador(tabuleiro, jogador, 13);
+    imprime_tabuleiro(tabuleiro, jogador);
+    if (!movimentacao)
+        printf("Bateu na parede! 🤕\n");
 
     destroi_tabuleiro(tabuleiro, jogador);
 }
